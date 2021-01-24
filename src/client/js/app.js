af1a2ec0b246f7c6add61f516d8e0ede46a1b63f
Husam Abuhabib <hmabuhabib@gmail.com>
2021-01-24 16:34:13 +0100

    add catch (error)

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -12,84 +12,115 @@ async function appfunction(event) {
 };
 
 async function geonames() {
+  try {
 
-  fetch('' + serverLink + '/geonames?city=' + cityInput.value + '')
-    .then(res => res.json())
-    .then(function (res) {
-      if (res.geonames.length > 0) {
-        Client.generateBtn.value = 'Loading ...'
-        Client.generateBtn.disabled = true;
-        Client.luckBtn.value = 'Loading ...'
-        Client.luckBtn.disabled = true;
-        tripInfo.city = cityInput.value
-        tripInfo.geonames = res.geonames[0]
-        tripInfo.startDate = Client.startDate.value
-        tripInfo.endDate = Client.endDate.value
-        tripInfo.howManyDaysWillStart = Client.numberOfDays(Client.today, Client.startDate.value)
-        tripInfo.longOfTheTrip = Client.numberOfDays(Client.startDate.value, Client.endDate.value)
-        return 1;
-      }
-      else {
-        alert('City not found')
-        return 1;
-      }
+    fetch('' + serverLink + '/geonames?city=' + cityInput.value + '')
+      .then(res => res.json())
+      .then(function (res) {
+        if (res.geonames.length > 0) {
+          Client.generateBtn.value = 'Loading ...'
+          Client.generateBtn.disabled = true;
+          Client.luckBtn.value = 'Loading ...'
+          Client.luckBtn.disabled = true;
+          tripInfo.city = cityInput.value
+          tripInfo.geonames = res.geonames[0]
+          tripInfo.startDate = Client.startDate.value
+          tripInfo.endDate = Client.endDate.value
+          tripInfo.howManyDaysWillStart = Client.numberOfDays(Client.today, Client.startDate.value)
+          tripInfo.longOfTheTrip = Client.numberOfDays(Client.startDate.value, Client.endDate.value)
+          return 1;
+        }
+        else {
+          alert('City not found')
+          return 1;
+        }
 
-    })
-    .then(weatherbit)
+      })
+      .then(weatherbit)
+
+
+  } catch (error) {
+    console.log("Error in geonames function", error);
+
+  }
+
+
+
 
 }
 
 
 async function weatherbit() {
 
-  fetch('' + serverLink + '/weatherbit?lat=' + tripInfo.geonames.lat + '&lon=' + tripInfo.geonames.lng + '&days=' + tripInfo.howManyDaysWillStart + '')
-    .then(res => res.json())
-    .then(function (res) {
-      tripInfo.weatherbit = res
-    })
-    .then(pixabay(tripInfo.city))
+  try {
+
+    fetch('' + serverLink + '/weatherbit?lat=' + tripInfo.geonames.lat + '&lon=' + tripInfo.geonames.lng + '&days=' + tripInfo.howManyDaysWillStart + '')
+      .then(res => res.json())
+      .then(function (res) {
+        tripInfo.weatherbit = res
+      })
+      .then(pixabay(tripInfo.city))
+
+  } catch (error) {
+    console.log("Error in weatherbit function", error);
+
+  }
 
 }
 
 async function pixabay(query) {
+  try {
+    fetch('' + serverLink + '/pixabay?place=' + query + '')
+      .then(res => res.json())
+      .then(function (res) {
+        if (res.total == 0) {
+          pixabay(tripInfo.geonames.countryName)
+        }
+        else {
+          tripInfo.pixabay = res
+          restcountries(tripInfo.geonames.countryName)
+        }
+      })
 
-  fetch('' + serverLink + '/pixabay?place=' + query + '')
-    .then(res => res.json())
-    .then(function (res) {
-      if (res.total == 0) {
-        pixabay(tripInfo.geonames.countryName)
-      }
-      else {
-        tripInfo.pixabay = res
-        restcountries(tripInfo.geonames.countryName)
-      }
-    })
+  } catch (error) {
+    console.log("Error in pixabay function", error);
+
+  }
 }
 
 async function restcountries(query) {
+  try {
+    fetch('' + serverLink + '/restcountries?name=' + query + '')
+      .then(res => res.json())
+      .then(function (res) {
+        tripInfo.restcountries = res
+        Client.tripsList.push(tripInfo)
+        printtripInfo()
+      }
+      )
 
-  fetch('' + serverLink + '/restcountries?name=' + query + '')
-    .then(res => res.json())
-    .then(function (res) {
-      tripInfo.restcountries = res
-      Client.tripsList.push(tripInfo)
-      printtripInfo()
-    }
-    )
+  } catch (error) {
+    console.log("Error in restcountries function", error);
+
+  }
 }
 
 
 
 async function printtripInfo() {
+  try {
+    if (!(tripInfo.weatherbit.max_temp)) {
 
-  if (!(tripInfo.weatherbit.max_temp)) {
+      setTimeout(printtripInfo, 2000)
 
-    setTimeout(printtripInfo, 2000)
+    }
+    else {
 
-  }
-  else {
+      Client.generateTripCard(Client.tripsList[Client.tripsList.length - 1])
+    }
+  } catch (error) {
+    console.log("Error in printtripInfo function", error);
 
-    Client.generateTripCard(Client.tripsList[Client.tripsList.length - 1])
   }
 
 }
